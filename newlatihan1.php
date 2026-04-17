a24d70ead56063f86f951da94cbd7810c54fbc24
Aadilla Candra Ningtias <19250915@bsi.ac.id>
2026-04-17 15:04:16 +0700

    latihan 1

---
+++ newlatihan1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan</title>
+</head>
+<body>
+    <h1>Latihan 1</h1>
+    <table>
+        <tr>
+            <th>Nama</th>
+            <th>Usia</th>
+            <th>No hp</th>
+        </tr>
+        <tr>
+            <td>John Doe</td>
+            <td>30</td>
+        </tr>
+        <tr>
+            <td>Aadilla</td>
+            <td>19</td>
+        </tr>
+    </table>
+</body>
+</html>
